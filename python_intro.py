c69d30c52ebef12997e54ee560c761b4ff6d3f2f
pedrolporto <pedrosivaldo@hotmail.com>
2023-12-29 16:29:27 -0300

    Meu primeiro comit DJANGO GIRLS APP

---
+++ python_intro.py
@@ -0,0 +1,21 @@
+# def ola(name):
+#     if(name==""):
+#         print("Olá estranho!")
+#     elif(name.lower() == ("Pedro".lower())):
+#         print("Oi, Pedro!")
+#     else:
+#         print("Te desconheço, retire-se!")
+def ola(name):
+    print("\nOlá, camarada "+name)
+
+# array_nomes = ["Pedro","Major","Dorotilde","Caio"]
+
+# for name in array_nomes:
+#     ola(name)
+    
+for i in range(0,10):
+    ola(str(i))
+    
+
+
+# ola("Pedro")
