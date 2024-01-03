e36cf7f749a46acd2a2f46037a7beb63e6fdf0fb
pedrolporto <pedrosivaldo@hotmail.com>
2024-01-03 10:23:00 -0300

    Melhorias no layout e adição de links para detalhes do post.

--- meusite/blog/urls.py
+++ meusite/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_post, name='listar_post'),
+    path('post/<int:pk>/', views.detalhe_post, name='detalhe_post'),
 ]
--- meusite/blog/views.py
+++ meusite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,7 @@ from .models import Post
 def listar_post(request):
     posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
     return render(request, 'blog/listar_post.html', {'posts': posts})
+
+def detalhe_post(request,pk):
+    post =  get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detalhe_post.html',{'post':post})
