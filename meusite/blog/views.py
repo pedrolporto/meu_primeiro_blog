b29185e66146b839d7b66ccf3346eea6b9e1d060
pedrolporto <pedrosivaldo@hotmail.com>
2024-01-02 23:41:14 -0300

    Posts dinâmicos de acordo com registros no DB.

--- meusite/blog/views.py
+++ meusite/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def listar_post(request):
-    return render(request, 'blog/listar_post.html', {})
+    posts = Post.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    return render(request, 'blog/listar_post.html', {'posts': posts})
